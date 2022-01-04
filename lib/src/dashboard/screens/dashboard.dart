fa73cd88b6c3a8682f9185edacd04596f46e31ee
AJAY KUMAR.V <ajaykumar.v@AJAYs-MacBook-Pro.local>
2022-01-04 17:50:32 +0530

    added submodule

---
+++ lib/src/dashboard/screens/dashboard.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class Dashboard extends StatelessWidget {
+  static const routeName = "/dashboard";
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+          child: const Text("dashboard"),
+        )
+    );
+  }
+}
